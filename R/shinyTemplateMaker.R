439e5aac127ec07caf5b2c5dd310fc69e63b5d44
tsuyuzaki <tsuyuzaki@macromill.com>
2018-09-19 11:55:02 +0900

    完成！

--- R/shinyTemplateMaker.R
+++ R/shinyTemplateMaker.R
@@ -1,6 +1,7 @@
 #' shinyTemplateMaker
 #'
 #' @import shiny
+#' @import shinythemes
 #' @name shinyTemplateMaker
 #' 
 
@@ -146,7 +147,7 @@ server <- function(input, output, session){
              "16" = stheme <- "united",
              "17" = stheme <- "yeti"
       )
-      ui.fluidpage.h <- paste0('fluidPage(theme = shinythemes::shinytheme("',stheme,'"),')
+      ui.fluidpage.h <- paste0('fluidPage(theme = shinytheme("',stheme,'"),')
     }
     ui.fluidpage.t <- ')'
     
